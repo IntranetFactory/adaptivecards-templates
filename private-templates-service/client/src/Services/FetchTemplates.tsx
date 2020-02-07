1e1783384c579027566583b216b0d23d1eb18957
Danielle Mott <t-damott@microsoft.com>
2020-02-06 16:53:40 -0800

    refactor

---
+++ private-templates-service/client/src/Services/FetchTemplates.tsx
@@ -0,0 +1,11 @@
+export default function retrieveCard(): any {
+  const url = "http://localhost:5000/template";
+  const token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IkhsQzBSMTJza3hOWjFXUXdtak9GXzZ0X3RERSJ9.eyJhdWQiOiIzOGZhZDdhOS1mZDhjLTQ3Y2MtOWMwYi02YzlmMzE3ODU3NjIiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vNzJmOTg4YmYtODZmMS00MWFmLTkxYWItMmQ3Y2QwMTFkYjQ3L3YyLjAiLCJpYXQiOjE1ODEwMzEwMjksIm5iZiI6MTU4MTAzMTAyOSwiZXhwIjoxNTgxMDM0OTI5LCJhaW8iOiI0Mk5nWUFqdThoSXRqTCszUGUvem1mczZYOWU3QUFBPSIsImF6cCI6IjM4ZmFkN2E5LWZkOGMtNDdjYy05YzBiLTZjOWYzMTc4NTc2MiIsImF6cGFjciI6IjEiLCJvaWQiOiJjODgyYjI4Yi1lOTNlLTQ5YmYtOGYyYi03ODAzZTFiY2QwMWIiLCJzdWIiOiJjODgyYjI4Yi1lOTNlLTQ5YmYtOGYyYi03ODAzZTFiY2QwMWIiLCJ0aWQiOiI3MmY5ODhiZi04NmYxLTQxYWYtOTFhYi0yZDdjZDAxMWRiNDciLCJ1dGkiOiJjVHYwNWR2bUtreXBfcmNwbkkwZEFBIiwidmVyIjoiMi4wIn0.UmroEcqUDATn38fUBMhsTJrcol_E0nvtN1FgR8SpEcByPJmGXWiiTP2pULvRp2yAspPDqQdXLVP6V8GlvniL-pyo92aEdUTSL7Z5-ZnwujLFEWW7-J4n8OvM1938ZqfriFZXlx0-_eJXSKM5cBvU6yl4IrzG6rARQrCV2i95RmvdH7Jikn6TvSSoK5QPMjfdNfn99qkQHZRb45UKYM0T59-GQt1hcDT3u1Ljz05FBKPueNlUS0ZI3umHmd9qIGwRYo6so1zWEeW5tOzznXq9yt5Z60Wa4YjR2YARMDrbiCdjL4Bo70B0AVfGiM_avZ8L7JCinuZqTTdxZ9xO_dzabw"
+  fetch(url, {
+    headers: {
+      Authorization: `${token}`
+    }
+  })
+    .then(res => res.text())
+    .then(json => console.log(json));
+}
